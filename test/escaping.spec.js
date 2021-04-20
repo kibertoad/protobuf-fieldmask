b4b245d72dbd6c198b402c06ddc790bd0d724125
Alexander Vainshtein <alexanderv@wix.com>
2021-04-20 12:38:02 +0300

    Introduce escaping utility

---
+++ test/escaping.spec.js
@@ -0,0 +1,60 @@
+const { assert } = require('chai');
+const { escape, unescapeAndSplit } = require('../lib/escaping');
+
+describe('Escaping', () => {
+  describe('escape', () => {
+    it('should return property name as is when it does not contain special characters', () => {
+      assert.equal(escape('foo'), 'foo');
+    });
+
+    it('should escape dots in the property name', () => {
+      assert.equal(escape('foo.bar'), 'foo\\.bar');
+    });
+
+    it('should escape multiple dots in the property name', () => {
+      assert.equal(escape('foo.bar.baz.'), 'foo\\.bar\\.baz\\.');
+    });
+
+    it('should escape the escape char', () => {
+      assert.equal(escape('foo\\bar'), 'foo\\\\bar');
+    });
+
+    it('should escape the escape char when containing with dots', () => {
+      assert.equal(escape('foo\\.bar'), 'foo\\\\\\.bar');
+    });
+  });
+
+  describe('unescapeAndSplit', () => {
+    it('should array with single element when path is for a single property', () => {
+      assert.deepEqual(unescapeAndSplit('foo'), ['foo']);
+    });
+
+    it('should return an array of names split by dot', () => {
+      assert.deepEqual(unescapeAndSplit('foo.bar.baz'), ['foo', 'bar', 'baz']);
+    });
+
+    it('should unescape escaped dots', () => {
+      assert.deepEqual(unescapeAndSplit('foo\\.bar'), ['foo.bar']);
+    });
+
+    it('should return an array of names split by dot with respect to escaped dots', () => {
+      assert.deepEqual(unescapeAndSplit('foo\\.bar\\.baz.baf'), ['foo.bar.baz', 'baf']);
+    });
+
+    it('should handle trailing escaped dot correctly', () => {
+      assert.deepEqual(unescapeAndSplit('foo\\..baz'), ['foo.', 'baz']);
+    });
+
+    it('should handle escape char correctly', () => {
+      assert.deepEqual(unescapeAndSplit('foo\\\\.baz'), ['foo\\', 'baz']);
+    });
+
+    it('should handle escape char with dot correctly', () => {
+      assert.deepEqual(unescapeAndSplit('foo\\\\\\..baz'), ['foo\\.', 'baz']);
+    });
+
+    it('should play well with escape function results', () => {
+      assert.deepEqual(unescapeAndSplit(escape('foo\\.bar')), ['foo\\.bar']);
+    });
+  });
+});
